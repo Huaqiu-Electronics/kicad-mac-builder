a9c887223e70b7730497100e92f16b229d87211c
Ethan Chien <liangtie.qian@gmail.com>
2025-04-24 22:58:14 +0800

    build(release): update release script for KiCAD 9.0.1
    - Update reference for symbols, footprints, and 3D packages to use version tag directly
    - Remove 'tag/' prefix for version references

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 ./build.py --arch=arm64 --release --target package-kicad-unified --kicad-ref release/9.0 \
-           --symbols-ref tag/9.0.1 --footprints-ref tag/9.0.1 \
-           --packages3d-ref tag/9.0.1 --templates-ref tag/9.0.1 \
+           --symbols-ref 9.0.1 --footprints-ref 9.0.1 \
+           --packages3d-ref 9.0.1 --templates-ref 9.0.1 \
            --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/9.0.1/kicad-doc-9.0.1.tar.gz \
            --release-name kicad-9.0.1-macos-arm64-copilot
