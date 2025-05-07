e410669ca869ddd50afb056a39f55fb1f2d89c3b
Ethan Chien <liangtie.qian@gmail.com>
2025-05-07 15:52:58 +0800

    build(release): update release script for 9.0.2 version
    - Update symbols, footprints, packages3D, and templates references to 9.0.2
    - Update docs tarball URL to use 9.0.2 version
    - Update release name to kicad-9

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 ./build.py --arch=arm64 --release --target package-kicad-unified --kicad-ref release/9.0 \
-           --symbols-ref 9.0.1 --footprints-ref 9.0.1 \
-           --packages3d-ref 9.0.1 --templates-ref 9.0.1 \
-           --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/9.0.1/kicad-doc-9.0.1.tar.gz \
-           --release-name kicad-9.0.1-macos-arm64-copilot
+           --symbols-ref 9.0.2 --footprints-ref 9.0.2 \
+           --packages3d-ref 9.0.2 --templates-ref 9.0.2 \
+           --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/9.0.2/kicad-doc-9.0.2.tar.gz \
+           --release-name kicad-9.0.2-macos-arm64-copilot
