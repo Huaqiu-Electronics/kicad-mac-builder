0dfb36c34d64df47c7328413bf7bb7cc03606c15
Ethan Chien <liangtie.qian@gmail.com>
2025-07-01 12:01:04 +0800

    ci(kicad): update release process and naming convention
    - Rename create_archive job to create_x86_64_archive
    - Update build and upload steps for x86_64 architecture
    - Modify release.sh to use dynamic tag and architecture
    - Remove release_x86_64.sh script
    - Update asset name to follow new naming convention

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
-./build.py --arch=arm64 --release --target package-kicad-unified --kicad-ref release/9.0 \
-           --symbols-ref 9.0.2 --footprints-ref 9.0.2 \
-           --packages3d-ref 9.0.2 --templates-ref 9.0.2 \
-           --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/9.0.2/kicad-doc-9.0.2.tar.gz \
-           --release-name kicad-huaqiu-9.0.2-macos-arm64
+TAG=${1:-9.0.2}      # Use first argument, or default to 9.0.2
+ARCH=${2:-arm64}     # Use second argument, or default to arm64
+
+./build.py --arch=$ARCH --release --target package-kicad-unified --kicad-ref release/9.0 \
+           --symbols-ref $TAG --footprints-ref $TAG \
+           --packages3d-ref $TAG --templates-ref $TAG \
+           --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/$TAG/kicad-doc-$TAG.tar.gz \
+           --release-name huaqiu-$TAG-macos-$ARCH
--- release_x86_64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-./build.py --arch=x86_64 --release --target package-kicad-unified --kicad-ref release/9.0 \
-           --symbols-ref 9.0.2 --footprints-ref 9.0.2 \
-           --packages3d-ref 9.0.2 --templates-ref 9.0.2 \
-           --docs-tarball-url https://gitlab.com/kicad/services/kicad-doc/-/archive/9.0.2/kicad-doc-9.0.2.tar.gz \
-           --release-name kicad-huaqiu-9.0.2-macos-arm64
