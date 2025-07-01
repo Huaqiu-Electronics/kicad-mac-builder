5486bf2a991195dcb913f05b133adad9534c4956
Ethan Chien <liangtie.qian@gmail.com>
2025-07-01 17:43:52 +0800

    ci(brew_deps): disable cmake update for now
    - Comment out cmake from BREW_DEPS list due to compatibility issues with kicad
    - Add FIXME comment to investigate how to get cmake 3.x for kiCAD

--- ci/src/brew_deps.sh
+++ ci/src/brew_deps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-export BREW_DEPS=(glew bison opencascade glm boost harfbuzz cairo doxygen gettext wget libgit2 libtool autoconf automake cmake swig openssl unixodbc ninja protobuf nng zstd libomp)
+# FIXME: cmake 4.x is not compatible with kicad.  Need to figure out how to get cmake 3.x
+# export BREW_DEPS=(glew bison opencascade glm boost harfbuzz cairo doxygen gettext wget libgit2 libtool autoconf automake cmake swig openssl unixodbc ninja protobuf nng zstd libomp)
+export BREW_DEPS=(glew bison opencascade glm boost harfbuzz cairo doxygen gettext wget libgit2 libtool autoconf automake swig openssl unixodbc ninja protobuf nng zstd libomp)
