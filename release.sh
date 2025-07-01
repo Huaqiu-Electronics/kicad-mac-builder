44b1de56ca4656060b24a3b40d0c34fe0e07b0fc
Ethan Chien <liangtie.qian@gmail.com>
2025-07-01 13:40:03 +0800

    build(release): skip Doxygen version check for release build
    - Set WX_SKIP_DOXYGEN_VERSION_CHECK=1 to bypass Doxygen version check
    - This change allows the release build to proceed without verifying the Doxygen version

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@
 TAG=${1:-9.0.2}      # Use first argument, or default to 9.0.2
 ARCH=${2:-arm64}     # Use second argument, or default to arm64
 
+export WX_SKIP_DOXYGEN_VERSION_CHECK=1  
+
 ./build.py --arch=$ARCH --release --target package-kicad-unified --kicad-ref release/9.0 \
            --symbols-ref $TAG --footprints-ref $TAG \
            --packages3d-ref $TAG --templates-ref $TAG \
